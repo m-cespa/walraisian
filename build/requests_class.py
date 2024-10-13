e1fb22b36290006221bd6e425409e9b86ff790bc
Sean Lim <sl2185@cam.ac.uk>
2024-10-13 17:37:25 +0100

    added logic for request object prices

--- build/requests_class.py
+++ build/requests_class.py
@@ -1,6 +1,5 @@
-import datetime
 class TicketRequest:
-    def __init__(self, timeID, userID, club, buy_or_sell, status, ticket_quantity):
+    def __init__(self, timeID, userID, club, buy_or_sell, status, ticket_quantity,price="market"):
         """
         Initialize a transaction object representing a user's ticket transaction.
 
@@ -11,6 +10,7 @@ class TicketRequest:
             buy_or_sell (str): Indicates whether the transaction is a 'buy' or 'sell'.
             status (bool): The current status of the transaction (True for completed, False for pending/cancelled).
             ticket_quantity (int): The number of tickets involved in the transaction.
+            price (float) : The asking/selling price of the ticket, if none, will get the market price from the resolve_price method
         """
         self.timeID = timeID
         self.userID = userID
@@ -18,5 +18,15 @@ class TicketRequest:
         self.buy_or_sell = buy_or_sell
         self.status = status
         self.ticket_quantity = ticket_quantity
-
+        self.price = self.resolve_price(price)
+    
+    def resolve_price(self,price):
+        if price == "market":
+            if self.buy_or_sell == "buy":
+                NotImplementedError # implement logic for calculating the market price
+            elif self.buy_or_sell == "sell":
+                NotImplementedError # implement logic for calculating the market price
+        return price
+    
+    
 
